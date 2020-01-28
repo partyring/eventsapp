e3c5e5ec1a82f5d1f35d83c1718ac76823ff86c6
Danielle Platt <danielleplatt@hotmail.co.uk>
2020-01-28 16:45:38 +0100

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Event;
+use Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $events = Event::where('user_id', Auth::id());
+
+        return view('home');
+    }
+}
